d471a9112ec3ec9cc0f4aadb68a86acb61f1b5a4
Luke Cook <invocatils@gmail.com>
2019-07-31 10:17:56 -0400

    changes

--- src/clojure/ernie/semantics.clj
+++ src/clojure/ernie/semantics.clj
@@ -7,7 +7,6 @@
 
 (defn verify
   [{:keys [environment] :as state} target params result]
-
   (try
     (if-let [verify-fn (get-in state [:methods target :verify])]
       (let [result (apply verify-fn result (walk/postwalk-replace environment params))]
@@ -17,33 +16,32 @@
       e)))
 
 (defn invoke-action
-  [{:keys [environment] :as state} action [_ force? target params :as exp]]
-  (try
-    (let [actuals (walk/postwalk-replace environment params)
-          result (apply action actuals)]
-      [:success (update state :executed conj [target actuals result]) result])
-    (catch IllegalArgumentException e
-      [:error (update state :failures conj {:expression exp
-                                            :error [:argument params]
-                                            :stack (get state :stack)})])
+  [{:keys [environment] :as state} action [_ target params :as exp]]
+  (let [actuals (walk/postwalk-replace environment params)]
+    (try
+      (let [result (apply action actuals)]
+        [:success (update state :executed conj [target actuals result]) result])
+      (catch IllegalArgumentException e
+        [:error (update state :failures conj {:expression exp
+                                              :error [:argument :action target params actuals]
+                                              :stack (get state :stack)})])
+      (catch Exception e
+        [:error (update state :failures conj {:expression exp
+                                              :error [:exception e]
+                                              :stack (get state :stack)})]))))
 
-    (catch Exception e
-      [:error (update state :failures conj {:expression exp
-                                            :error [:exception e]
-                                            :stack (get state :stack)})])))
-
-(defn take-action
-  [{:keys [environment] :as state} [_ force? target params :as exp]]
+(defn eval|action
+  [{:keys [environment] :as state} [_ target params :as exp]]
   (if-let [action (get-in state [:methods (keyword target) :action])]
     (let [[status state return :as result] (invoke-action state action exp)]
       (if (failure? result)
         result
         (let [verification (verify state target params return)]
-          (if (true? verification)
+          (if (= true verification)
             [:success state return]
             [:failure
               (update state :failures conj {:expression exp
-                                            :error [:verification target params]
+                                            :error [:verification target (walk/postwalk-replace environment params)]
                                             :stack (get state :stack)})
              verification]))))
     [:failure (update state :failures conj
@@ -51,32 +49,19 @@
                        :error [:undefined :action target]
                        :stack (get state :stack)})]))
 
-(defn take-and-cache-action
-  [state [_ force? target params :as exp]]
-  (let [[status state result] (take-action state exp)]
-    [status (assoc-in state [:cache target params] [status result]) result]))
-
-(defn eval|action
-  [state [_ force? target params :as exp]]
-  (if force?
-    (take-and-cache-action state exp)
-    (if-let [[status result] (get-in state [:cache target params])]
-      (if (verify state target params result)
-        [status state result]
-        (take-and-cache-action state exp))
-      (take-and-cache-action state exp))))
-
 (defn eval|expect
   [state [_ exp expected]]
-  (let [[status :as result] (eval|exp state exp)
+  (let [[status state :as result] (eval|exp state exp)
         expected (or expected :success)]
-    (if (= status :error)
+    (if (= expected :success)
       result
-      (if (= status expected)
+      (if (= status :error)
         result
-        [:failure (update state :failures conj {:expression exp
-                                                :error [:expected expected (first result)]
-                                                :stack (get state :stack)})]))))
+        (if (= status expected)
+          result
+          [:failure (update state :failures conj {:expression exp
+                                                  :error [:expected expected (first result)]
+                                                  :stack (get state :stack)})])))))
 
 (defn eval|bind
   [state [_ sym exp]]
@@ -114,14 +99,6 @@
                                           :error [:undefined :case name]
                                           :stack (get state :stack)})]))
 
-(defn eval|call-oob
-  [{:keys [environment] :as state} [_ name actuals expected :as exp]]
-  (if-let [case (get-in state [:cases (keyword name)])]
-    (invoke-case state case (walk/postwalk-replace environment actuals))
-    [:error (update state :failures conj {:expression exp
-                                          :error [:undefined :case name]
-                                          :stack (get state :stack)})]))
-
 (defn eval|case
   [state [_ name formals & body :as case]]
   [:success (update state :cases assoc (keyword name) case)])
@@ -131,7 +108,6 @@
   (condp = (first exp)
     :case       (eval|case       state exp)
     :call       (eval|call       state exp)
-    :call-oob   (eval|call-oob   state exp)
     :bind       (eval|bind       state exp)
     :expect     (eval|expect     state exp)
     :action     (eval|action     state exp)))
